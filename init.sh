11fabea9121b89d1191fcffa770808c2d1ccabe8
linhvoyo <linhvoyo@ymail.com>
2018-02-16 23:03:48 -0800

    working on nmap

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
-
-
-#V2
+#V2 Network
 
 # https://www.cyberciti.biz/faq/linux-list-network-interfaces-names-command/
 1. ip link | egrep "^[0-9]*: " | cut -d " " -f2 | sed s'/:$//'
@@ -14,9 +12,28 @@
 # https://serverfault.com/questions/47915/how-do-i-get-the-default-gateway-in-linux-given-the-destination
 4. ip route show default| awk '/default/ {print $3}'
 
-#still looking
-5.
-
+#https://www.thegeekstuff.com/2012/07/nslookup-examples/?utm_source=tuicool
+*5. nslookup slash16.org | head -n1 | cut -d ":" -f2 | tr -d "[:blank:]"
 
 #https://www.cyberciti.biz/faq/linux-unix-find-out-dns-server-ip-address-names/
-6. 
+6. /etc/resolv.conf
+
+#https://www.computerhope.com/unix/unslooku.htm
+*7. nslookup slash16.org $(nslookup -type=any slash16.org | grep nameserver| head -n1 | cut -d "=" -f2 | tr -d "[:blank:]")
+
+#https://hostadvice.com/tools/whois/#slash16.org
+8. The web hosting provider for slash16.org is Amazon Web service
+
+#nslookup 42.fr | grep "Address: " | cut -d " " -f2
+9.  163.172.250.12
+    163.172.250.11
+
+# https://itsfoss.com/how-to-find-what-devices-are-connected-to-network-in-ubuntu/
+# https://superuser.com/questions/311019/display-list-of-computers-on-a-lan-in-linux
+#https://www.howtogeek.com/190148/8-common-network-utilities-explained/
+#https://hackertarget.com/nmap-cheatsheet-a-quick-reference-guide/
+10.
+
+
+#
+12. ping -c1 10.51.1.253
--- init_notes.sh
+++ init_notes.sh
@@ -4,3 +4,6 @@ eval $(docker-machine env default)
 docker pull debian
 #docker run command
 docker run -it --rm -v $(pwd):/data debian
+#intall nslookup
+apt-get update
+apt-get install -y dnsutils
