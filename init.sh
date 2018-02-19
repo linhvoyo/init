0d67e4cd3354e42bafef08dd987b2600a63fe511
linhvoyo <linhvoyo@ymail.com>
2018-02-19 13:16:14 -0800

    starting scripts

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 1. ip link | egrep "^[0-9]*: " | cut -d " " -f2 | sed s'/:$//'
 
 # https://www.cyberciti.biz/faq/linux-ip-command-examples-usage-syntax/ip-command-show-address-ethernet-interface/
-2. ip addr show eth0 | grep inet | cut -d " " -f 8
+*2. ip addr show eth0 | grep inet | cut -d " " -f 8
 
 # https://superuser.com/questions/239701/how-can-i-find-out-my-wifi-adapters-mac-address-on-ubuntu-linux
 3. ip addr show eth0 | grep link/ether  | cut -d " " -f 6
@@ -32,8 +32,59 @@
 # https://superuser.com/questions/311019/display-list-of-computers-on-a-lan-in-linux
 #https://www.howtogeek.com/190148/8-common-network-utilities-explained/
 #https://hackertarget.com/nmap-cheatsheet-a-quick-reference-guide/
-10.
-
+10. traceroute slash16.org
 
 #
-12. ping -c1 10.51.1.253
+12. ping -c2 10.51.1.253
+
+15. /etc/hosts
+
+
+#V3
+1. /etc/debian_version
+
+#https://www.cyberciti.biz/faq/ubuntu-change-hostname-command/
+# https://github.com/moby/moby/issues/8902#issuecomment-218911749
+#
+# need to install sudo
+#apt-get install -y sudo
+2. hostname new.machine.name
+
+3. /etc/hostname
+
+#http://osxdaily.com/2009/09/22/check-your-macs-uptime-and-reboot-history/
+4. uptime
+
+8. ~/.ssh/authorized_keys
+
+9. who
+
+# https://ubuntuforums.org/archive/index.php/t-2054455.html
+10. fdisk -l
+
+11. df -h
+
+12. du -h /var
+
+13. top
+
+14. tail -f /var/log/syslog &
+
+#https://unix.stackexchange.com/questions/104821/how-to-terminate-a-background-process
+15. pkill tail
+
+#https://www.ibm.com/developerworks/library/l-job-scheduling/
+16. cron
+
+# apt-get install -y iptables
+17. iptables -L
+
+#https://crm.vpscheap.net/knowledgebase.php?action=displayarticle&id=29
+#https://www.garron.me/en/bits/iptables-open-port-for-specific-ip.html
+18. iptables -I INPUT -p tcp -s 10.0.0.0/8 --dport 22 -j ACCEPT
+
+#https://unix.stackexchange.com/questions/11851/iptables-allow-certain-ips-and-block-all-other-connection
+19. iptables -I INPUT -p tcp ! -s 10.0.0.0/8 --dport 22 -j DROP
+
+
+#V3
--- init_notes.sh
+++ init_notes.sh
@@ -4,6 +4,18 @@ eval $(docker-machine env default)
 docker pull debian
 #docker run command
 docker run -it --rm -v $(pwd):/data debian
+docker run -it --rm -v $(pwd):/data  --cap-add SYS_ADMIN  debian:jessie
 #intall nslookup
 apt-get update
 apt-get install -y dnsutils
+
+iptables
+
+
+#need this for iptables
+docker run -it --rm -v $(pwd):/data  --cap-add=SYS_ADMIN --cap-add=NET_ADMIN  debian:jessie
+
+#build containter as root
+https://medium.com/@mccode/processes-in-containers-should-not-run-as-root-2feae3f0df3b
+
+docker run -it --rm -v $(pwd):/data  --privileged debian:jessie
